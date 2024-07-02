334378ae87dcad6b4b113abd27a96fbe9aa49b70
abhijitnayak <abhijit.nayak@globallogic.com>
2024-07-02 14:23:15 +0530

    solving problem on Concurrency Utilities.

--- src/tpas/gl/training/concurrency/utilities/Problem2.java
+++ src/tpas/gl/training/concurrency/utilities/Problem2.java
@@ -1,7 +1,47 @@
 package tpas.gl.training.concurrency.utilities;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class Problem2 {
     public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(3, () -> {
+            System.out.println("Barrier point reached. All threads have passed the barrier.");
+        });
+
+        // Create three threads that will wait at the barrier point
+        Thread thread1 = new Thread(new Workerr(barrier));
+        Thread thread2 = new Thread(new Workerr(barrier));
+        Thread thread3 = new Thread(new Workerr(barrier));
+
+        // Start all three threads
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+// Worker class that waits at the barrier point
+class Workerr implements Runnable {
+    private final CyclicBarrier barrier;
+
+    public Workerr(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " is waiting at the barrier point.");
+            // Wait at the barrier point
+            barrier.await();
+
+            // After passing the barrier point, simulate some work with Thread.sleep
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + " has completed its task.");
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
 
     }
 }
